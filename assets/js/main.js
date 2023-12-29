f6d224a7f2eb92106232c483f39415bd6a2f576e
DiaaSharqawi <dodosharqawi2023@gmail.com>
2023-12-29 23:55:36 +0200

    JS First Task commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+document.querySelector('div').classList.add('demo');
+console.log(document.querySelector('div').classList);
+
+document.querySelector('h1').classList.add('vip');
+console.log(document.querySelector('h1').classList);
+document.querySelector('h1').classList.remove('vip');
+console.log(document.querySelector('h1').classList);
+
+
+
+document.querySelector('p').classList.add('footerCopyRight');
+console.log(document.querySelector('p').classList);
